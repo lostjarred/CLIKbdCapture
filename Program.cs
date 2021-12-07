ad210726c6d3fdc32a9b1a0264b9afab45cab8fa
lostjarred <lostotaku@outlook.com>
2021-12-07 18:14:54 +1100

    add comments

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ namespace KBDCommandLineCapture
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
+            //xy for welcome msg
             int[] kbdwelcomxy = new int[2] { 0, 1 };
+            //xy for key output
             int[] kbdkeeyxy = new int[2] {0, 3};
+            //xy for whether the key was captured
             int[] kbdstatusxy = new int[2] {0, 5};
             System.ConsoleKey keypress;
 
+            //initial screen draw before loop
             Console.SetCursorPosition(kbdwelcomxy[0], kbdwelcomxy[1]);
             Console.Write("Welcome to my Keyboard Capture Program");
             Console.SetCursorPosition(kbdkeeyxy[0], kbdkeeyxy[1]);
@@ -19,19 +23,24 @@ namespace KBDCommandLineCapture
             Console.SetCursorPosition(kbdstatusxy[0], kbdstatusxy[1]);
             Console.Write("> NO KEY");
             
+            //main loop
             while (true)
             {
                 keypress = 0;
+                //read keypress
                 keypress = Console.ReadKey(false).Key;
+                //redraw screen method
                 redrewscreen();
             }
 
             void redrewscreen()
             {
+                //clear the screen
                 Console.Clear();
                 Console.SetCursorPosition(kbdwelcomxy[0], kbdwelcomxy[1]);
                 Console.Write("Welcome to my Keyboard Capture Program");
                 Console.SetCursorPosition(kbdkeeyxy[0], kbdkeeyxy[1]);
+                //logic for if a key was captured
                 if (keypress == 0)
                 {
                     Console.Write(" ");
@@ -41,6 +50,7 @@ namespace KBDCommandLineCapture
                     Console.Write(keypress + "(" + ((int)keypress) +")");
                 }
                 Console.SetCursorPosition(kbdstatusxy[0], kbdstatusxy[1]);
+                //logic for status bar at bottom
                 if (keypress == 0)
                 {
                     Console.Write("> NO KEY");
